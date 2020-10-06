d0227ea07c04711266a9b9d66415817173162c92
Marcos Querino <61765516+MQ-J@users.noreply.github.com>
2020-10-06 14:22:00 -0300

    PAGUE com card or money
    três opções de pagamento: dinheiro, cartão, cheque.

--- PAGUE.c
+++ PAGUE.c
@@ -6,7 +6,8 @@
 
 /*VARIÁVEIS*/
 char lc_all;
-int pagament;
+char pagament, op;
+int op_card, conta_digito;
 int tam;
 FILE * arq;
 int preco_total;
@@ -25,16 +26,48 @@ int main()
 	while (!feof(arq))
 	fscanf(arq, "%i", &preco_total);
 	fclose(arq);
-	
 	printf("Preço total: R$ %i", preco_total); 
-	printf("\n\nO Pagamento foi realizado? [s=sim] [n=não]\n");
-	fflush (stdin);
+	
+	printf("\n\nQual a forma de pagamento?");
+	printf("\n\n a = dinheiro\n b = cartão de débito/crédito\n c = cheque\n\n");
+	fflush(stdin);
 	scanf ("%c", &pagament);
-	if (pagament =='s')                                         /* pagamento concluído*/
-	system ("LEVE");
-	if (pagament =='n')                                       /* pagamento NÃO concluído */
+	switch (pagament)
 	{
-	remove ("lista_pedido.txt");
-	system ("MENU");
+		case 'a': case 'A': case 'c': case 'C':
+		printf("Pagamento concuído?\n");
+		fflush(stdin);
+		scanf ("%c", &op);
+		if (op == 's')                                         /*pagamento concluído*/
+		system ("LEVE");
+		if (op =='n')                                       /* pagamento NÃO concluído */
+		{
+			remove ("lista_pedido.txt");
+			system ("MENU");
+		}
+		break;
+		
+		case 'b': case 'B':
+		conta_digito = 0;
+		printf("Digite o número do cartão: ");
+		fflush(stdin);
+		do
+		{
+			op_card = getch();
+			printf("*");
+			conta_digito++;
+		}
+		while (conta_digito <= 15);
+		printf("\nPagamento concuído?\n");
+		fflush(stdin);
+		scanf ("%c", &op);
+		if (op == 's')                                         /*pagamento concluído*/
+		system ("LEVE");
+		if (op =='n')                                       /* pagamento NÃO concluído */
+		{
+			remove ("lista_pedido.txt");
+			system ("MENU");
+		}
+		break;
 	}
 }
